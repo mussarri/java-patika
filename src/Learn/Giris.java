1515454431d85a629c99b278ae9b73f4124efd6e
musarri <msaricicek_99@hotmail.com>
2024-03-13 22:29:30 +0300

    first commit

---
+++ src/Learn/Giris.java
@@ -0,0 +1,13 @@
+package Learn;
+import java.util.Scanner;
+
+public class Giris {
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.print("A sayisiini giriniz: ");
+        a = input.nextInt();
+        System.out.println(a);
+
+    }
+}
