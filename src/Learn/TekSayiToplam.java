97a40940fc97c9a343b573fd8947280bc8ddf080
musarri <msaricicek_99@hotmail.com>
2024-03-15 01:07:13 +0300

    tek sayi toplayan program

--- src/Learn/TekSayiToplam.java
+++ src/Learn/TekSayiToplam.java
@@ -1,2 +1,25 @@
-package Learn;public class TekSayiToplam {
+package Learn;
+
+import java.util.Scanner;
+
+public class TekSayiToplam {
+    public static void main(String[] args) {
+        int x, toplam=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sayiyi giriniz: ");
+        x = input.nextInt();
+        if (x > 0){
+            for (int i=0; i < x ;i++){
+                if (i % 2 ==1 ){
+                    toplam = toplam + i;
+                }
+
+            }
+            System.out.println(x + " e kadar olan tek sayilarin toplami = " + toplam);
+        }else{
+            System.out.println("0 dan buyuk birsayi giriniz");
+        }
+
+
+    }
 }
