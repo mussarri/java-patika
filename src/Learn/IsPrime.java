e6baa4acbc394b224721114f492790102047cc70
musarri <msaricicek_99@hotmail.com>
2024-08-26 19:01:41 +0300

    isprime with recursive

--- src/Learn/IsPrime.java
+++ src/Learn/IsPrime.java
@@ -1,16 +1,35 @@
 package Learn;
 
+import java.util.Scanner;
+
 public class IsPrime {
 	
-	static boolean isPrime(int a) {
-		if(a==2) return true;
+	static void isPrime(int a, int b) {
 		
-		return isPrime(a - 1 );
-		 
+		if(b > a/2) {
+			System.out.println("Prime");
+			return;
+		}
+		if(a % b == 0) {
+			System.out.println("Not prime");
+			return;
+		}
+		
+		isPrime(a, b + 1);
+ 
 	}
 	
+	static void isPrime(int a) {
+		isPrime(a, 2);	 
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println(isPrime(3));
+		Scanner input = new Scanner(System.in);
+		System.out.print("Is prime this number: ");
+		int num = input.nextInt();
+		isPrime(num);
+		
 	}
 
 }
