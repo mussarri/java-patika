1034e497df021a6881ef78a70990d6fd6b7d9c8b
musarri <msaricicek_99@hotmail.com>
2024-08-30 20:46:06 +0300

    array iterable elements

--- arrays/src/arrays/Ntimes.java
+++ arrays/src/arrays/Ntimes.java
@@ -3,13 +3,15 @@ package arrays;
 public class Ntimes {
 	public static void main(String[] args) {
 		int[] array = { 10, 20, 39, 10, 20, 10, 40, 50 };
+		
 		for (int i = 0; i < array.length; i++) {
 			int times = 1;
 			for (int j = 0; j < array.length; j++) {
 				if (i == j) {
 					continue;
 				}
-				if (array[i] == array[j]) {
+				if (array[i] == array[j] && array[j] != -1) {
+					array[j] = -1;
 					times++;
 				}
 				
